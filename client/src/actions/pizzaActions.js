d1ae6991678d0841e746645ba231f49257fe3b75
tomburnside <tomburnside90@gmail.com>
2023-03-21 18:12:09 +1100

    chnaged for production

--- client/src/actions/pizzaActions.js
+++ client/src/actions/pizzaActions.js
@@ -4,7 +4,7 @@ export const getAllPizzas=()=>async dispatch=>{
     dispatch({type:'GET_PIZZAS_REQUEST'})
 
     try {
-        const response = await axios.get('http://localhost:5000/api/pizzas/getallpizzas')
+        const response = await axios.get('/api/pizzas/getallpizzas')
         console.log(response);
         dispatch({type:'GET_PIZZAS_SUCCESS' , payload : response.data})
        
